77fb53dcd0a27d6cfad1ee2b7c1f483113286334
shinae Yoo <104437077+shin-ramen-yoo@users.noreply.github.com>
2022-05-26 15:33:11 +0000

    Part 1 - I added the counter of the numbertosquare into the while loop so that the loop would continue and end.

--- part5.py
+++ part5.py
@@ -18,3 +18,14 @@ Enter a number or enter 0 to stop: 0
 Number of evens: 3
 
 '''
+num = int(input("Enter a number or enter 0 to stop: "))
+evencounter = 0
+
+while num != 0:
+  if num%2 == 1:
+    num = int(input("Enter a number or enter 0 to stop: "))
+  if num%2 == 0:
+    evencounter = evencounter + 1
+    num = int(input("Enter a number or enter 0 to stop: "))
+
+print("Number of evens: " + str(evencounter))
